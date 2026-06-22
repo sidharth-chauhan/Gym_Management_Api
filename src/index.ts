80b7914f94d1f8e08968c4ed504b3fe0c7cbc655
Sidharth chauhan <chauhansiddharth71@gmail.com>
2026-06-22 15:37:40 +0530

    connect DB

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import {connectDB} from '../database';
 
 
-const app=express()
 
-app.listen(3000,()=>{
-    console.log('Server is running on port 3000');
+dotenv.config();
+connectDB();
+const app=express();
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Server is running on port ${process.env.PORT}`);
 })
