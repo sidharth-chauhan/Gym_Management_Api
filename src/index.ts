3ea2007309ed4b6cb2afe3ee2ba6bd3423c9bfdc
Sidharth chauhan <chauhansiddharth71@gmail.com>
2026-06-24 23:40:54 +0530

    added route

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,16 @@ import express from 'express';
 import dotenv from 'dotenv';
 import {connectDB} from '../database';
 import User from './models/User';
+import authRoutes from './Routes.ts/authRoutes';
 
 
 
 dotenv.config();
 connectDB();
 const app=express();
+app.use(express.json());
+
+app.use("/api/user",authRoutes) 
 
 
 app.listen(process.env.PORT,()=>{
