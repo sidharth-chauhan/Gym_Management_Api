9c035706143922a1c889ea50836a37f59a4352cb
Sidharth chauhan <chauhansiddharth71@gmail.com>
2026-07-07 22:51:57 +0530

    added comments

--- src/Routes.ts/gymRoutes.ts
+++ src/Routes.ts/gymRoutes.ts
@@ -7,8 +7,12 @@ import { verify } from "node:crypto";
 const router=Router()
 
 
+//general
 router.get("/profile",verifyToken, getProfile);
 router.get("/dashboard",verifyToken,dashboard)
+
+
+//membership
 router.post("/memberships",verifyToken,addMembership)
 router.get("/memberships",verifyToken,getMemberships)
 router.get("/memberships/:id",verifyToken,getMembershipById)
@@ -16,6 +20,7 @@ router.patch("/memberships/:id",verifyToken,updateMembership)
 router.delete("/memberships/:id",verifyToken,removeMembership)
 
 
+//trainer
 router.post("/trainers",verifyToken,addTrainer)
 router.get("/trainers",verifyToken,getTrainer)
 router.get("/trainers/:id",verifyToken,getTrainerById)
@@ -23,10 +28,12 @@ router.patch("/trainers/:id",verifyToken,updateTrainer)
 router.delete("/trainers/:id",verifyToken,removeTrainer)
 
 
+//member
 router.post("/members",verifyToken,addMember)
 router.get("/members",verifyToken,getMembers)
 router.get("/members/:id",verifyToken,getMemberById)
 router.patch("/members/:id",verifyToken,updateMember)
 router.delete("/members/:id",verifyToken,removeMember)
+
 
 export default router;
